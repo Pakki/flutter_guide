8a17cfdb48a2fbcb2962c2d71e62144a928e311c
Andrey Pakki <pakki.a@yandex.ru>
2022-03-28 15:49:47 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //throw UnimplementedError();
+    return MaterialApp(
+      title: 'helloworld)',
+      home: Text('Hello!'),
+    );
+  }
+}
